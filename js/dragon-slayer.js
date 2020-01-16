f57f9161c227c023a34ba5494dbaa4b6765bad6c
Adnen-Ben-Abdelaali <benabdelaaliadnen@gmail.com>
2020-01-16 14:15:19 +0100

    final commit

--- js/dragon-slayer.js
+++ js/dragon-slayer.js
@@ -3,11 +3,27 @@
 /*************************************************************************************************/
 /* **************************************** DONNEES JEU **************************************** */
 /*************************************************************************************************/
+var msgDragonAttaquantAvant = '<figure class="game-round"><img src="images/dragon-winner.png" alt="Dragon vainqueur"><figcaption>';
+var msgDragonAttaquantApres = '</figcaption></figure>';
+var msgJoueurAttaquantAvant = '<figure class="game-round"><img src="images/knight-winner.png" alt="Chevalier vainqueur"><figcaption>';
+var msgJoueurAttaquantApres = '</figcaption></figure>';
 
+
+var msgVaniqueur = 'Vous avez gagné ,Le Dragon vous supplie de le laisser en vie !';
+var msgJoueurVainqueurAvant = '<figure class="game-round"><img src="images/knight-winner.png" alt="Chevalier vainqueur"><figcaption>';
+var msgDragonVainqueurApres = '</figcaption></figure>';
+
+var msgPerdant = 'Vous avez perdu le combat, le dragon vous a carbonisé !';
+var msgDragonVainqueurAvant = '<footer><h3>Fin de la partie</h3><figure class="game-end"><figcaption>';
+var msgDragonVainqueurApres = '</figcaption><img src="images/dragon-winner.png" alt="Dragon vainqueur"></figure></footer>';
+
+
+var dernierAttaquantDragon = false;
 var nombreDeViesJoueur = 0;
 var nombreDeViesDragon = 0;
 var niveauDeLaPartie ='';
-
+var pointsDeDommage = 0;
+var messageSuivantAttaquant = '';
 function  initiGame() {
 
   var niveauxPossibles = new Array('FACILE', 'NORMAL', 'DIFFICILE', 'F', 'N', 'D', '1', '2', '3');
@@ -24,7 +40,7 @@ function  initiGame() {
 
 function getRandomNumber(min, max) {
 
-  return min + Math.trunc(Math.random() * (max - min + 1));
+  return Math.floor(min + Math.random() * (max - min + 1));
 }
 
 
@@ -63,6 +79,26 @@ En mode difficile :
 --> Affichage des PV de départ
 */
 
+function affichageMiniatureDragonJoueur() {
+
+  var messagePointsDeVieJoueur ="";
+  var messagePointsDeVieDragon ="";
+
+  if(nombreDeViesJoueur <= 0) {
+    messagePointsDeVieJoueur = "GAME OVER";
+    messagePointsDeVieDragon = nombreDeViesDragon + " PV";
+  }else if(nombreDeViesDragon <= 0) {
+    messagePointsDeVieDragon = "GAME OVER";
+    messagePointsDeVieJoueur = nombreDeViesJoueur + " PV";
+  }else {
+    messagePointsDeVieDragon = nombreDeViesDragon + " PV";
+    messagePointsDeVieJoueur = nombreDeViesJoueur + " PV";
+  }
+    document.write('<div class="game-state">' + '<figure class="game-state_player">' + '<img src="images/knight.png" alt="Chevalier">');
+    document.write('<figcaption>' + messagePointsDeVieJoueur + ' </figcaption>' + '</figure>' + '<figure class="game-state_player">' + '<img src="images/dragon.png" alt="Dragon">' + '<figcaption>' + messagePointsDeVieDragon +' </figcaption>' + '</figure></div>');
+}
+
+
 function initPointsDeVie() {
 
   let niveauDeLaPartie = initiGame();
@@ -93,51 +129,21 @@ function initPointsDeVie() {
       document.write("ERREUR !");
   }
 
+  affichageMiniatureDragonJoueur();
 
 }
 
-initPointsDeVie();
-document.write('<div class="game-state">' + '<figure class="game-state_player">' + '<img src="images/knight.png" alt="Chevalier">' + '<figcaption>' + nombreDeViesJoueur + ' PV</figcaption>' + '</figure>' + '<figure class="game-state_player">' + '<img src="images/dragon.png" alt="Dragon">' + '<figcaption>' + nombreDeViesDragon +' PV</figcaption>' + '</figure></div>');
+function makeLiveZero(live) {
+  if (live < 0) {
+    live = 0;
+  }
+  return 0;
+}
 
-/*
-
-A chaque tour de jeu, les étapes suivantes ont lieu :
-
-1) On détermine qui est le plus rapide et attaque l'autre : c'est l'initiative
-
-	Calcul de l'initiative : chaque personnage lance 10D6. Celui qui a le plus grand score attaque l'autre.
-
-2) On détermine le nombre de points de dommage causés par l'attaquant à son adversaire
-
-	a) Si c'est le dragon qui attaque :
-
-		Les points de dommages sont égaux à 3D6.
-		Ensuite ces points de dommages sont majorés ou minorés en fonction de la difficulté et de la classe du héro.
-
-		* Niveau facile : les points de dommages sont minorés de 2D6%.
-		* Niveau difficile : les points de dommages sont majorés de 1D6%.
-
-	b) Si c'est le héro qui attaque
-
-		Les points de dommages sont égaux à 3D6.
-		Ensuite ces points de dommages sont majorés ou minorés en fonction de la difficulté et de la classe du héro.
-
-		* Niveau facile : les points de dommages sont majorés de 2D6%.
-		* Niveau difficile : les points de dommages sont minorés de 1D6%.
-
-3) Affichage du journal du jeu : que s'est-il passé pendant le tour ?
-
-    - Affichage du numéro du tour
-	- Affichage de qui a attaqué et combien de points de dommages ont été causés
-	- Affichage de l'état du jeu
-
-Fin de la partie
-----------------
-*/
 
 function playTheGame() {
 
-  var tour = 0;
+  var tour = 1;
   var attaqueDragon = 0;
   var attaqueJoueur = 0;
 
@@ -148,28 +154,36 @@ function playTheGame() {
 
     if(attaqueDragon > attaqueJoueur) {
 
+      dernierAttaquantDragon = true;
+
       switch(niveauDeLaPartie.toUpperCase()) {
 
         case 'FACILE':
         case 'F':
         case '1':
-          nombreDeViesJoueur -= (getRandomLives(3,6) - (getRandomLives(3,6) / 100));
+          pointsDeDommage = (getRandomLives(3,6) - Math.floor((getRandomLives(3,6) / 100)));
+          nombreDeViesJoueur -= pointsDeDommage;
           break;
 
         case 'NORMAL':
         case 'N':
         case '2':
-          nombreDeViesJoueur -= (getRandomLives(3,6) - (getRandomLives(3,6) / 100));
+          pointsDeDommage = (getRandomLives(3,6) - Math.floor((getRandomLives(3,6) / 100)));
+          nombreDeViesJoueur -= pointsDeDommage;
           break;
 
         case 'DIFFICILE':
         case 'D':
         case '3':
-          nombreDeViesJoueur -= (getRandomLives(3,6) + (getRandomLives(1,6) / 100));
+          pointsDeDommage = (getRandomLives(3,6) + Math.floor((getRandomLives(1,6) / 100)));
+          nombreDeViesJoueur -= pointsDeDommage;
+
           break;
         default:
           document.write("ERREUR !");
       }
+      messageSuivantAttaquant = "Le dragon prend l'initiative, vous attaque et vous inflige " + pointsDeDommage + "points de dommage !";
+      document.write('<h3>Tour n°' + tour + '</h3>' + msgDragonAttaquantAvant + messageSuivantAttaquant + msgDragonAttaquantApres);
 
 
 
@@ -180,65 +194,63 @@ function playTheGame() {
         case 'FACILE':
         case 'F':
         case '1':
-          nombreDeViesDragon -= (getRandomLives(3,6) + (getRandomLives(2,6) / 100));
+          pointsDeDommage = (getRandomLives(3,6) + Math.floor((getRandomLives(2,6) / 100)));
+          nombreDeViesDragon -= pointsDeDommage;
           break;
 
         case 'NORMAL':
         case 'N':
         case '2':
-          nombreDeViesDragon -= (getRandomLives(3,6) + (getRandomLives(2,6) / 100));
+          pointsDeDommage = (getRandomLives(3,6) + Math.floor((getRandomLives(2,6) / 100)));
+          nombreDeViesDragon -= pointsDeDommage;
           break;
 
         case 'DIFFICILE':
         case 'D':
         case '3':
-          nombreDeViesDragon -= (getRandomLives(3,6) - (getRandomLives(1,6) / 100));
+          pointsDeDommage = (getRandomLives(3,6) - Math.floor((getRandomLives(1,6) / 100)));
+          nombreDeViesDragon -= pointsDeDommage;
+
           break;
         default:
           document.write("ERREUR !");
       }
+      messageSuivantAttaquant = "Vous êtes le plus rapide, vous attaquez le dragon et lui infligez" + pointsDeDommage + "points de dommage !";
+      document.write('<h3>Tour n°' + tour + '</h3>' + msgJoueurAttaquantAvant + messageSuivantAttaquant + msgJoueurAttaquantApres);
+
     }
+/*
+    makeLiveZero(nombreDeViesDragon);
+    makeLiveZero(nombreDeViesJoueur);
 
-    attaqueDragon = 0;
-    attaqueJoueur = 0;
-
-
-
-
-        <img src="images/knight-winner.png" alt="Chevalier vainqueur">
-        <figcaption>Vous êtes le plus rapide, vous attaquez le dragon et lui infligez 30 points de dommage !</figcaption>
-    </figure>
-
-    <div class="game-state">
-        <figure class="game-state_player">
-            <img src="images/knight.png" alt="Chevalier">
-            <figcaption>124 PV</figcaption>
-        </figure>
-        <figure class="game-state_player">
-            <img src="images/dragon.png" alt="Dragon">
-            <figcaption>105 PV</figcaption>
-        </figure>
-    </div>
-
-    document.write('<h3>Tour n°' + tour + '</h3>' + '<figure class="game-round">');
+*/
+    affichageMiniatureDragonJoueur();
 
     tour++;
+    attaqueDragon = 0;
+    attaqueJoueur = 0;
+    pointsDeDommage = 0;
+    messageSuivantAttaquant = '';
 
   }
 
-/*
-) Affichage du journal du jeu : que s'est-il passé pendant le tour ?
+  if(dernierAttaquantDragon)  {
 
-    - Affichage du numéro du tour
-  - Affichage de qui a attaqué et combien de points de dommages ont été causés
-  - Affichage de l'état du jeu
+    document.write(msgDragonVainqueurAvant + msgPerdant + msgDragonVainqueurApres);
 
-Fin de la partie
-----------------
-
-*/
+  }else if(!dernierAttaquantDragon) {
+    document.write(msgJoueurVainqueurAvant + msgVaniqueur + msgDragonVainqueurApres);
+  }
 
 }
+
+initPointsDeVie();
+
+playTheGame();
+
+
+
+
 
 
 
